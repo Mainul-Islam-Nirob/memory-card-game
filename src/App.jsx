3f0b25104defad617f5f3399832ed3cf7ba58342
Mainul Islam <maainul.islam@gmail.com>
2025-04-07 00:24:50 +0600

    Set up tailwindcss and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+// App.jsx
+import React from "react";
+import Card from "./components/Card";
+import Scoreboard from "./components/Scoreboard";
 
-function App() {
-
+ function App() {
+ 
   return (
-    <>
-     <h1>Test your brain capacity!!!</h1>
-    </>
-  )
+    <div className="p-4 text-center">
+      <h1 className="text-3xl font-bold mb-4">😺 Cat Memory Game</h1>
+      <Scoreboard />
+      <div className="grid grid-cols-4 gap-4 mt-6">
+        <Card />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Scoreboard() {
+  return (
+    <div>
+      score.......
+    </div>
+  )
+}
+
+export default Scoreboard
